583b88bcdd777b7385033d5731e1551eefff604d
Hopson97 <mhopson@hotmail.co.uk>
2018-04-07 12:01:15 +0100

    begin work on main menu

---
+++ Source/States/StateMainMenu.cpp
@@ -0,0 +1,22 @@
+#include "StateMainMenu.h"
+
+StateMainMenu::StateMainMenu(Game & game)
+    :   StateBase(game)
+{
+}
+
+void StateMainMenu::handleEvent(sf::Event e)
+{
+}
+
+void StateMainMenu::handleInput()
+{
+}
+
+void StateMainMenu::update(sf::Time deltaTime)
+{
+}
+
+void StateMainMenu::render(sf::RenderTarget & renderer)
+{
+}
---
+++ Source/States/StateMainMenu.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "StateBase.h"
+#include "../GUI/StackMenu.h"
+
+class StateMainMenu : public StateBase
+{
+    public:
+        StateMainMenu(Game& game);
+
+        void handleEvent(sf::Event e)   override;
+        void handleInput()  override;
+        void update(sf::Time deltaTime) override;
+        void render(sf::RenderTarget& renderer) override;
+
+    private:
+    
+};
