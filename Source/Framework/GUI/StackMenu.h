4aa87fd1294d5f013086dd40600e09351b9cea14
Hopson97 <mhopson@hotmail.co.uk>
2019-06-03 22:46:12 +0100

    stack menu doc begin

--- Source/Framework/GUI/StackMenu.h
+++ Source/Framework/GUI/StackMenu.h
@@ -1,3 +1,13 @@
+/**
+ * @file StackMenu.h
+ * @author Matthew Hopson
+ * @brief Class definition for a stack menu
+ * @version 0.1
+ * @date 2019-06-03
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #pragma once
 
 #include <SFML/Graphics.hpp>
@@ -7,44 +17,45 @@
 #include "Widget.h"
 #include "../Util/NonCopyable.h"
 
-namespace gui
-{
+namespace gui {
+    /**
+     * @brief Class for a stack menu
+     * A stack menu is a menu where its widgets appear in a vertical stack
+     */
+    class StackMenu : public NonCopyable {
+        public:
+            StackMenu(const sf::RenderWindow& window, float baseY);
+            StackMenu(const sf::Vector2f& position);
 
-class StackMenu : public NonCopyable
-{
-    public:
-        StackMenu(const sf::RenderWindow& window, float baseY);
-        StackMenu(const sf::Vector2f& position);
+            StackMenu(StackMenu&& other);
+            StackMenu& operator =(StackMenu&& other);
 
-        StackMenu(StackMenu&& other);
-        StackMenu& operator =(StackMenu&& other);
+            ~StackMenu() = default;
 
-        ~StackMenu() = default;
+            void addWidget(std::unique_ptr<Widget> w);
+            /*
+            template<typename T, typename... Args>
+            void addWidget(Args&&... args) {
+                auto w = std::make_unique<T>(std::forward<Args>(args)...);
+                initWidget(*w);
+                m_widgets.push_back(std::move(w));
+            }*/
 
-        void addWidget(std::unique_ptr<Widget> w);
-        /*
-        template<typename T, typename... Args>
-        void addWidget(Args&&... args) {
-            auto w = std::make_unique<T>(std::forward<Args>(args)...);
-            initWidget(*w);
-            m_widgets.push_back(std::move(w));
-        }*/
+            void setTitle(const std::string& title, const sf::RenderTarget& target);
 
-        void setTitle(const std::string& title, const sf::RenderTarget& target);
+            void handleEvent   (sf::Event e, const sf::RenderWindow& window);
+            void render        (sf::RenderTarget& renderer);
 
-        void handleEvent   (sf::Event e, const sf::RenderWindow& window);
-        void render        (sf::RenderTarget& renderer);
+        private:
+            void initWidget(Widget& w);
 
-    private:
-        void initWidget(Widget& w);
+            std::vector<std::unique_ptr<Widget>> m_widgets;
+            sf::RectangleShape m_background;
 
-        std::vector<std::unique_ptr<Widget>> m_widgets;
-        sf::RectangleShape m_background;
+            sf::Vector2f m_basePosition;
+            sf::Vector2f m_baseSize;
 
-        sf::Vector2f m_basePosition;
-        sf::Vector2f m_baseSize;
-
-        Widget::Text m_titleText;
-};
+            Widget::Text m_titleText;
+    };
 
 }
