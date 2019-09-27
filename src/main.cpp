99893dbe4b52f18c38e9c83320a6602921fae139
Hopson97 <mhopson@hotmail.co.uk>
2019-09-27 21:01:44 +0100

    Begin work on the cmake improvements

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,69 @@
-#include "input/keyboard.h"
-#include <SFML/Graphics.hpp>
+#include "Game.h"
+#include "SpaceInvaders/States/StateMainMenu.h"
+
+#include <iostream>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+namespace {
+    void setConsolePosition()
+    {
+#ifdef _WIN32
+        HWND consoleWindow = GetConsoleWindow();
+        SetWindowPos(consoleWindow, 0, 0, 0, 500, 500,
+                     SWP_NOSIZE | SWP_NOZORDER);
+#endif
+    }
+
+    constexpr int GAMES = 1;
+
+    template <typename T> void run()
+    {
+        std::cout << "\nTo choose another Game, simply close the window\n";
+        std::cin.ignore();
+        Game app{};
+        app.initGame<T>();
+        app.run();
+        std::cout << '\n';
+    }
+
+    bool isValidChoice(int option) { return option > 0 && option <= GAMES + 1; }
+} // namespace
 
 int main()
 {
-    sf::RenderWindow window({1280, 720}, "SFML", sf::Style::Default);
-    window.setFramerateLimit(60);
-    window.setKeyRepeatEnabled(false);
+    setConsolePosition();
+    bool exit = false;
+    while (!exit) {
+        int option = 0;
+        std::cout << "Which Game would you like to play?\n"
+                  << "1. Space Invaders\n"
+                  << GAMES + 1 << ". Exit\n";
 
-    Keyboard keyboard;
+        while (!isValidChoice(option)) {
+            std::cin >> option;
 
-    while (window.isOpen()) {
-        sf::Event e;
-        while (window.pollEvent(e)) {
-            keyboard.update(e);
-            switch (e.type) {
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-                default:
-                    break;
+            if (!isValidChoice(option)) {
+                std::cout
+                    << "Invalid option, please pick a number between 1 and "
+                    << GAMES + 1 << "\n";
+                std::cin.ignore();
             }
         }
-        window.clear();
 
-        window.display();
+        switch (option) {
+            case 1:
+                run<SpaceInvaders::StateMainMenu>();
+                break;
+
+            case GAMES + 1:
+                exit = true;
+                break;
+
+            default:
+                break;
+        }
     }
 }
