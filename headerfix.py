88efbb1cee52d0fe3b480da2da5e0b14da250e34
Hopson97 <mhopson@hotmail.co.uk>
2019-09-28 10:56:36 +0100

    Add script to add namespace arcade to files

--- headerfix.py
+++ headerfix.py
@@ -2,11 +2,21 @@ from pathlib import Path
 import re
 import os
 
-pathlist = Path('src').glob('**/*.h')
+pathlist = Path('src/arcade').glob('**/*.h')
 for path in pathlist:
     with open(path) as file:
+        print ("__--------------------------________")
+        fileContent = ""
+        found = False
         for line in file:
-            if line.startswith('#include') and "arcade" in line:
-                i = line.find('arcade')
-                line = "#include <" + line[i:-2] + ">"
-                print(line)
+            if not line.startswith('#include') and not line.startswith('#pragma') and not "*" in line and not line.isspace() and not found:
+                fileContent += 'namespace arcade {\n'
+                found = True
+
+            fileContent += line
+        fileContent += '}'
+        print (fileContent)
+        with open(path, 'w') as out:
+            out.write(fileContent)
+
+
